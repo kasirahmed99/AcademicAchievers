be4a17e0709570d88ff3b0c4abc22dc963d07f7a
Kasir Ahmed Sayem <kasirahmed9999@outlook.com>
2025-12-07 19:54:31 +0100

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.academicachievers"
-    compileSdk = 35
+    compileSdk = 36   
 
     defaultConfig {
         applicationId = "com.example.academicachievers"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
